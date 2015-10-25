99eb9d15bd8d1a7bfad9c639cd8f819a9c2df687
jgreenlee24 <jgreenlee@harding.edu>
2015-10-25 17:32:58 -0500

    "All Music" updates DataGrid

--- hTunes/MainWindow.xaml.cs
+++ hTunes/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -33,6 +34,49 @@ namespace hTunes
         {
             InitializeComponent();
             musicLib = new MusicLib();
+            InitializeList();
         }
+
+        private void InitializeList(){
+            listBox1.Items.Add("All Music");
+            foreach (string playlist in musicLib.Playlists)
+            {
+                listBox1.Items.Add(playlist);
+            }
+            listBox1.SelectedValue = "All Music";
+            UpdateDataGrid();
+        }
+
+        private void UpdateDataGrid()
+        {
+            string playlist_name = (string)listBox1.SelectedValue;
+            if (playlist_name == "All Music")
+            {
+                foreach (string id in musicLib.SongIds)
+                {
+                    Song song = musicLib.GetSong(Int32.Parse(id));
+                    DataGridItem item = new DataGridItem();
+                    item.id = song.Id;
+                    item.title = song.Title;
+                    item.artist = song.Artist;
+                    item.album = song.Album;
+                    item.genre = song.Genre;
+                    dataGrid.Items.Add(item);
+                }
+            }
+            else
+            {
+                dataGrid.ItemsSource = musicLib.SongsForPlaylist(playlist_name).AsDataView();
+            }
+        } 
+    }
+
+    public struct DataGridItem
+    {
+        public int id { get; set;}
+        public string title { get; set; }
+        public string artist { get; set; }
+        public string album { get; set; }
+        public string genre { get; set; }
     }
 }
